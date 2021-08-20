3607e8da8e752a80faf9be12c159bb5f282b8979
Roi malka <roimalka10@gmail.com>
2021-08-20 19:12:13 +0300

    added scss and created first navbar with responsive design

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const handleClick = () => {
+    setShowMenu(!showMenu);
+  };
+  return (
+    <>
+      <div className="navbar">
+        <a href="/">Herolo Weather App</a>
+        <div className={showMenu ? "navbar-items active" : "navbar-items"}>
+          <NavLink activeClassName="activated" to="/">
+            <span>Home</span>
+          </NavLink>
+          <NavLink activeClassName="activated" to="/favourites">
+            <span>Favourites</span>
+          </NavLink>
+        </div>
+        <div className="burger-icon" onClick={handleClick}>
+          {showMenu ? <FaTimes size={32} /> : <FaBars size={28} />}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
